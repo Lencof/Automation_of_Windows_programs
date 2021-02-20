65315af27648c131494059071ac576703c418d10
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-20 14:20:24 +0300

    Update fast_open_Google.py

--- fast_open_Google.py
+++ fast_open_Google.py
@@ -2,6 +2,7 @@
 # fast_open_Google.py
 
 if __name__ == '__main__':
-    import webbrowser
+    import webbrowser # use webbrowser
     
-    
+    # Will return True and open a tab
+    webbrowser.open_new_tab("https://google.com") # your site
