ac85a71d0d5860440cb53953401db48df94bb5fb
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-20 14:16:02 +0300

    Update fast_open_Github.py

--- fast_open_Github.py
+++ fast_open_Github.py
@@ -1,1 +1,8 @@
+# __Author__ __Lencof__
+# fast_open_Github.py
 
+if __name__ == '__main__'
+    import webbrowser # use webbrowser
+    
+    # Will return True and open a tab
+    webbrowser.open_new_tab("https://github.com/Lencof")   
