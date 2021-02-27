83a50cb9b932094c53d8703edff3546c89170e74
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-27 10:11:45 +0300

    Update automation_cmd_ping.py

--- automation_cmd_ping.py
+++ automation_cmd_ping.py
@@ -1,4 +1,13 @@
 # __Author__ __Lencof__
 # automation_cmd_ping.py
+'''
+this script checks the ping of the site, 
+in other words, it poisons ping requests
+'''
 
-import os
+import os # use os
+import webbrowser # use webbrowser
+ 
+# create class Ping(): 
+class Ping():
+   os.system("cmd.exe/ping/google.com") # your site 
