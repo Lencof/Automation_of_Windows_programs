735a95e37e1c445cdc91157ea466733958284d88
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-28 10:36:34 +0300

    Create fast_open_Facebook.py

---
+++ fast_open_Facebook.py
@@ -0,0 +1,8 @@
+# __Author__ __Lencof__
+# fast_open_Facebook.py
+
+if __name__ = '__main__':
+    import webbrowser # use webbrowser
+    
+    # Will return True and open a tab
+    webbrowser.open_new_tab("https://web.facebook.com") # your site  
