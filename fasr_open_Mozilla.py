fb3514ea4dde0e8a3e682f846b09e8faf92eccba
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-28 10:31:23 +0300

    Update fasr_open_Mozilla.py

--- fasr_open_Mozilla.py
+++ fasr_open_Mozilla.py
@@ -1,1 +1,8 @@
+# __Author__ __Lencof__
+# fasr_open_Mozilla.py
 
+if __name__ = '__main__':
+    import webbrowser # use webbrowser
+  
+# Will return True and open a tab
+webbrowser.open_new_tab("https://www.mozilla.org/") # your site
