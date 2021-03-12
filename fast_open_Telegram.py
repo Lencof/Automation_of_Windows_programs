f5c3c35dc22f5eb029776860fc01c4a8cec70294
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-12 08:51:15 +0300

    Create fast_open_Telegram.py

---
+++ fast_open_Telegram.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# fast_open_Telegram.py
+
+import os # use os
+import sys # use sys
+
+class Telegram(): # create class Telegram():
+  pass # an empty block
+
+os.system('Telegram.exe') # your programm
