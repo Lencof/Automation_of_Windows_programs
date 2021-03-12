bebe1eae6185d1be0024b4fcf9ee410c00288a47
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-12 08:47:26 +0300

    Create fast_open_Winrar.py

---
+++ fast_open_Winrar.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# fast_open_Winrar.py
+
+import os # use os 
+# import sys
+
+class Winrar(): # create class Winrar():
+  pass # an empty block
+
+os.system('Winrar.exe') # your programm
