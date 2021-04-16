9fd49c107c4a2b259422910b8c6704f4fca927b3
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-16 11:15:19 +0300

    Update fast_open_Python.org.py

--- fast_open_Python.org.py
+++ fast_open_Python.org.py
@@ -1,5 +1,6 @@
 # __Author__ __Lencof__
 # fast_open_Python.org.py
 
-import webbrowser # use webbrowser
+import os 
+import webbrowser 
 webbrowser.open_new_tab('https://www.python.org') # your site
