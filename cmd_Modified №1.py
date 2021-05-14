4a828a31f6569551d00f754dc28c34e9a1562427
Lencof <71154457+Lencof@users.noreply.github.com>
2021-05-14 12:44:53 +0300

    Update cmd_Modified №1.py

--- cmd_Modified №1.py
+++ cmd_Modified №1.py
@@ -1,7 +1,9 @@
 # __Author__ __Lencof__
 # cmd_Modified №1.py
 
-import os # use os
+import os 
+import sys
+import os.path
 
 class CMD(): # create class CMD():
    print(os.system("cmd .exe")) # your programm
