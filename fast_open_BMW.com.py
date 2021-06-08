58264184f9b1cf94ee50a29e4e40dc16bcb6e73a
Lencof <71154457+Lencof@users.noreply.github.com>
2021-06-08 12:31:21 +0300

    Update fast_open_BMW.com.py

--- fast_open_BMW.com.py
+++ fast_open_BMW.com.py
@@ -1,9 +1,6 @@
 # __Author__ __Lencof__
 # fast_open_BMW.com.py
 
-import os 
-import sys 
-import os.path
 import webbrowser
 
 os.system('/usr/bin/https://www.bmw.com/') # your site and programm. 
