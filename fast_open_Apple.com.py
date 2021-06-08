f36da274816c9679dab390b6a0ea12c51e7b4f00
Lencof <71154457+Lencof@users.noreply.github.com>
2021-06-08 12:30:53 +0300

    Update fast_open_Apple.com.py

--- fast_open_Apple.com.py
+++ fast_open_Apple.com.py
@@ -1,9 +1,6 @@
 # __Author__ __Lencof__
 # fast_open_Apple.com.py
 
-import os 
-import sys
-import os.path
 import webbrowser
 
 os.system('/usr/bin/https://www.apple.com/') # your site and programm. 
