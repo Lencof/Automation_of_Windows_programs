4db66f5f04c1c654bf12832f6054a0fdde8ddca4
Lencof <71154457+Lencof@users.noreply.github.com>
2021-06-08 12:31:07 +0300

    Update fast_open_Avast.com.py

--- fast_open_Avast.com.py
+++ fast_open_Avast.com.py
@@ -1,9 +1,6 @@
 # __Author__ __Lencof__
 # fast_open_Avast.com.py
 
-import os 
-import sys 
-import os.path
 import webbrowser
 
 os.system('/usr/bin/https://www.avast.com/') # your site and programm. 
