1445f932c09f0ab1fab9cc0e7f20e536fd66c30a
Lencof <71154457+Lencof@users.noreply.github.com>
2021-06-08 12:31:34 +0300

    Update fast_open_Booking.com.py

--- fast_open_Booking.com.py
+++ fast_open_Booking.com.py
@@ -1,9 +1,6 @@
 # __Author__ __Lencof__
 # fast_open_Booking.com.py
 
-import os 
-import sys 
-import os.path
 import webbrowser
 
 os.system('/usr/bin/https:/www.booking.com//') # your site and programm. 
