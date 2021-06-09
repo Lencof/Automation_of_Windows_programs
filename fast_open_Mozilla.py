56ac472f1f76228446af3853fb868b6554f5fdf8
Lencof <71154457+Lencof@users.noreply.github.com>
2021-06-09 04:06:19 +0300

    Update fast_open_Mozilla.py

--- fast_open_Mozilla.py
+++ fast_open_Mozilla.py
@@ -2,9 +2,7 @@
 # fasr_open_Mozilla.py
 
 if __name__ = '__main__':
-    import webbrowser # use webbrowser
-    import sys
-    import os
+    import webbrowser 
   
 # Will return True and open a tab
 webbrowser.open_new_tab("https://www.mozilla.org/") # your site
