f4e7547b863b2b05f50308167b5bcd4292fcc05f
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-05 10:14:21 +0300

    Update fast_open_PayPal.py

--- fast_open_PayPal.py
+++ fast_open_PayPal.py
@@ -1,7 +1,5 @@
 # __Author__ __Lencof__
 # fast_open_PayPal.py
 
-import webbrowser # use webbrowser
-
-if __name__ == '__main__':
-    webbrowser.open_new_tab('https://www.paypal.com') # your site
+import webbrowser 
+webbrowser.open_new_tab('https://www.paypal.com') # your site
