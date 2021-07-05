52735d66a98596d874a959de57a226055d198c73
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-05 10:13:52 +0300

    Update fast_open_Mozilla.py

--- fast_open_Mozilla.py
+++ fast_open_Mozilla.py
@@ -1,8 +1,5 @@
 # __Author__ __Lencof__
 # fasr_open_Mozilla.py
 
-if __name__ = '__main__':
-    import webbrowser 
-  
-# Will return True and open a tab
+import webbrowser 
 webbrowser.open_new_tab("https://www.mozilla.org/") # your site
