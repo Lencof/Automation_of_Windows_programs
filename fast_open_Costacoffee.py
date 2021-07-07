f4a26ca08b22ee0c088476c3bb09602672b8759b
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-07 08:49:14 +0300

    Update fast_open_Costacoffee.py

--- fast_open_Costacoffee.py
+++ fast_open_Costacoffee.py
@@ -2,4 +2,4 @@
 # fast_open_Costacoffee.py
 
 import webbrowser
-webbrowser.open_new_tab('https://www.costacoffee.com/') # your site and programm.
+webbrowser.open_new_tab('https://www.costacoffee.com/') 
